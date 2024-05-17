606767a8fef2a37ce74852cae5a5f2f412d298c1
DSA - Eli Mendonça <eli.mendonca@adventistas.org>
2024-05-17 08:14:55 -0300

    Atualiza tema p/ a campanha de 2024

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ require_once 'vendor/autoload.php';
 require_once(dirname(__FILE__) . '/classes/controllers/PA_CPT_Projects.class.php');
 require_once(dirname(__FILE__) . '/classes/controllers/PA_CPT_SliderHome.class.php');
 require_once(dirname(__FILE__) . '/classes/controllers/PA_Enqueue_Files.class.php');
-require_once(dirname(__FILE__) . '/classes/controllers/PA_CPT_Magazine.class.php');
+// require_once(dirname(__FILE__) . '/classes/controllers/PA_CPT_Magazine.class.php');
 
 
 add_action('after_setup_theme', function () {
